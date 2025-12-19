0c8aa5f3ef75a5f46e22c1b14a32a1693921ee4e
Joshua-2333 <jaflores.2333@gmail.com>
2025-12-19 16:34:25 -0600

    quick update to point to correct file path for categoryRouter.js

--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -6,7 +6,8 @@ router.get('/:id', async (req, res) => {
   try {
     const category = await getCategoryById(req.params.id);
     const items = await getItemsByCategory(req.params.id);
-    res.render('categories/show', { category, items });
+    // Render the existing categoryDetail.ejs
+    res.render('categories/categoryDetail', { category, items });
   } catch (err) {
     console.error(err);
     res.status(500).send('Server error');
